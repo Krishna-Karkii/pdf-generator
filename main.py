468d4d78a523b619357f84f4d05923fbf387def1
krishna <krishnakarki1996@gmail.com>
2024-05-01 20:14:06 +0545

    create lines in pdf

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pandas
 from fpdf import FPDF
 import pandas as pd
 
+# lines = [23,33,43,53,63,73,83,93,103,113,123,133,143,153,163,173,183
+#         ,193,203,213,223,233,243,253,263,273]
+
 pdf = FPDF(orientation='p', unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
@@ -16,7 +19,7 @@ for index, rows in dv.iterrows():
     pdf.cell(w=0, h=13, txt=rows["Topic"], ln=1, align='L')
 
     # Giving the co-ordinates of the line
-    pdf.line(10,23,200,23)
+    # pdf.line(10,23,200,23)
 
     # Add the footer
     pdf.ln(265)
@@ -24,8 +27,14 @@ for index, rows in dv.iterrows():
     pdf.set_font(family="Helvetica", style="I", size=10)
     pdf.cell(h=10, w=0, ln=1, align="R", txt=rows["Topic"])
 
+    for y in range(23, 273, 10):
+        pdf.line(10, y, 200, y)
+
     for i in range(rows['Pages'] - 1):
         pdf.add_page()
+
+        for y in range(23, 273, 10):
+            pdf.line(10, y, 200, y)
 
         # Add the footer
         pdf.ln(277)
