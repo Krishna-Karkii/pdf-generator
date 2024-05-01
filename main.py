dae1dc09bf54d77321d5386cca9234c86b2e938a
krishna <krishnakarki1996@gmail.com>
2024-05-01 16:37:57 +0545

    create nested loop for multi-pages

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ for index, rows in dv.iterrows():
     pdf.set_fill_color(254, 0, 0)
     pdf.set_font(family="Helvetica", style="B", size=25)
     pdf.cell(w=0, h=13, txt=rows["Topic"], ln=1, align='L')
+
+    # Giving the co-ordinates of the line
     pdf.line(10,23,200,23)
+    for i in range(rows['Pages'] - 1):
+        pdf.add_page()
+
 
 pdf.output('output.pdf')
 
