9afb5c8c8994f7e64c550447ab9bc8d9cf8296d3
krishna <krishnakarki1996@gmail.com>
2024-05-01 15:22:05 +0545

    create multi-pages pdf

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
+import pandas
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation='p', unit="mm", format="A4")
-pdf.add_page()
 
-pdf.set_font(family="Helvetica", style="B", size=12)
-pdf.cell(w=0, h=13, txt="Uno Reverse", ln=1, align='T')
+dv = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Helvetica", style="I", size=12)
-pdf.cell(w=0, h=13, txt="Wanna play uno reverse", ln=1, align="L")
+for index, rows in dv.iterrows():
+    pdf.add_page()
 
-pdf.add_page()
+    pdf.set_fill_color(254, 0, 0)
+    pdf.set_font(family="Helvetica", style="B", size=25)
+    pdf.cell(w=0, h=13, txt=rows["Topic"], ln=1, align='L')
+    pdf.line(10,23,200,23)
 
-pdf.set_font(family="Helvetica", style="B", size=12)
-pdf.cell(w=0, h=13, txt="Uno Reverse", ln=1, align='T')
-
-pdf.set_font(family="Helvetica", style="I", size=12)
-pdf.cell(w=0, h=13, txt="Wanna play uno reverse", ln=1, align="L")
-
-pdf.output('uno.pdf')
+pdf.output('output.pdf')
 
 
